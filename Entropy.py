a782f468fa622258b18ca0d617cd98b0be1aa6d8
Jingchao Yang <jcyangt@gmail.com>
2018-03-23 12:28:12 -0400

    updated with more testing sets

--- Entropy.py
+++ Entropy.py
@@ -15,14 +15,34 @@ def calcShannonEnt(dataSet):
 
     for type in typeCounts:
         prob = float(typeCounts[type])/countDataSet
+        # print prob
         shannonEnt -= prob * log(prob,2)
-
 
     return shannonEnt
 
+dataSet1 = [['Chinese'],['Chinese']]
+dataSet2 = [['Chinese'], ['Japanese']]
+dataSet3 = [['Chinese'], ['Japanese'], ['American']]
 
-dataSet1 = [['Chinese'], ['Chinese'], ['Japan'], ['Chinese'], ['Japan']]
-dataSet2 = [['Chinese'], ['Italian'], ['Japan'], ['Chinese'], ['Japan']]
+############ entropy for base set ##############
+dataSet4 = [['Chinese'], ['Japanese'], ['Italian'], ['American']]
 
-print calcShannonEnt(dataSet1)
-print calcShannonEnt(dataSet2)
+############ entropy after enlarge set data ##############
+dataSet5 = [['Chinese'], ['Chinese'], ['Japanese'], ['Italian'], ['American']]
+dataSet6 = [['Chinese'], ['Japanese'], ['Japanese'], ['Italian'], ['American']]
+dataSet7 = [['Chinese'], ['Japanese'], ['Italian'], ['Italian'], ['American']]
+dataSet8 = [['Chinese'], ['Japanese'], ['Italian'], ['American'], ['American']]
+# dataSet9 = [['Chinese'], ['Japanese'], ['Italian'], ['American'],['Chines'], ['Japanes'], ['Italia'], ['America']]
+
+data = [dataSet1,dataSet2,dataSet3,dataSet4,dataSet5,dataSet6,dataSet7,dataSet8]
+
+for d in data:
+    print calcShannonEnt(d)
+
+# due to Chinese food are more similar to Japanese food than Italian to American
+# print '############ with weight ##############'
+# print calcShannonEnt(dataSet5) - 0.2
+# print calcShannonEnt(dataSet6) - 0.2
+# print calcShannonEnt(dataSet7) - 0.1
+# print calcShannonEnt(dataSet8) - 0.1
+
